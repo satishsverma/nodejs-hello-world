8217531db6d0ba0a02c7a786309922ddb3f2f552
Satish Verma <satishsverma@gmail.com>
2019-09-18 15:10:07 +0530

    update hostname

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 var http = require('http');
+var os = require("os");
+var hostname = os.hostname();
 
 var server = http.createServer(function(request, response) {
 
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello World!!!!");
+    response.end("Hello World!!!!"+"\n\nHOSTNAME="+hostname);
 
 });
 
